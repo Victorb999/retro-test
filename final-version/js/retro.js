55a8579a322e462ad99e04bb50f27ba35f97fa55
Victor Araújo <victor.b_@hotmail.com>
2024-12-09 16:04:31 -0300

    test final

---
+++ final-version/js/retro.js
@@ -0,0 +1,276 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container")
+  const errorMessage = document.querySelector(".error-message")
+
+  // Verifica se o dispositivo é mobile ou se o navegador suporta a API de compartilhamento
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+
+  // FUNÇÃO PARA DECODIFICAR OS PARÂMETROS
+  const getDecodedParams = () => {
+    const params = new URLSearchParams(window.location.search)
+    const base64Data = params.get("dados")
+    if (!base64Data) return null
+
+    try {
+      const decoded = atob(base64Data)
+      const utf8Decoded = decodeURIComponent(encodeURIComponent(decoded)) // Corrige caracteres UTF-8
+      return JSON.parse(utf8Decoded)
+    } catch (error) {
+      console.error("Erro ao decodificar os parâmetros:", error)
+      return null
+    }
+  }
+
+  // SETA OS PARAMS
+  const params = getDecodedParams()
+
+  // Array com nome dos campos esperados no JSON
+  const rules = [
+    "name",
+    "cyc",
+    "tot24",
+    "line1",
+    "line2",
+    "line3",
+    "mut",
+    "cpv",
+    "ekos",
+    "kaik",
+    "ref",
+    "avn",
+    "mov",
+    "hgh",
+    "camp",
+    "cod",
+    "level",
+  ]
+
+  // Verifica se o dispositivo é mobile ou se o navegador suporta a API de compartilhamento
+  if (!isMobile || !navigator.canShare) {
+    document.querySelectorAll(".sharebtnbase").forEach((btn) => {
+      btn.innerHTML = '<p class="cor1">Baixar imagem</p>'
+    })
+  }
+
+  // Preenche os slides com os dados
+  const populateSlides = () => {
+    if (!params) {
+      container.style.display = "none"
+      errorMessage.style.display = "block"
+      return
+    }
+    rules.forEach((key) => {
+      //const slide = panels[slideIndex - 1]; // Ajusta o índice para ser baseado em 0
+
+      const div = document.getElementById(key)
+      if (div && params[key]) {
+        div.textContent = `${params[key]}` // Preenche o conteúdo da div
+      }
+    })
+    document.getElementById("name-2").textContent = params.name
+  }
+
+  // Verifica se o valor é válido
+  const isValid = (value) => {
+    return (
+      value !== undefined &&
+      value !== null &&
+      value !== "" &&
+      value !== "null" &&
+      value !== "NULL" &&
+      value !== "Null"
+    )
+  }
+
+  // regras para esconder os cards
+  const rulesShow = () => {
+    const removeElement = (id) => {
+      const element = document.getElementById(id)
+      if (element) {
+        element.remove()
+      }
+    }
+    //CARD 2- PRODUTOS
+    if (params.cyc < 8) removeElement("section-Produtos-cyc")
+    if (params.tot24 < 50) removeElement("section-Produtos-tot24")
+    if (params.cyc < 8 && params.tot24 < 50) {
+      removeElement("card-Produtos")
+      removeElement("img-Produtos")
+    }
+
+    //CARD 3 - LINHAS
+    if (!isValid(params.line1)) removeElement("section-Linhas-line1")
+    if (!isValid(params.line2)) removeElement("section-Linhas-line2")
+    if (!isValid(params.line3)) removeElement("section-Linhas-line3")
+    if (
+      !isValid(params.line1) &&
+      !isValid(params.line2) &&
+      !isValid(params.line3)
+    ) {
+      removeElement("card-Linhas")
+      removeElement("img-Linhas")
+    }
+
+    //CARD 4 - MUTUALIDADE
+    if (!isValid(params.mut)) {
+      removeElement("img-Mutualidade")
+      removeElement("card-Mutualidade")
+    }
+
+    //CARD 5 - CPV
+    if (params.cpv <= 0) {
+      removeElement("img-CPV")
+      removeElement("card-CPV")
+    }
+    if (params.cpv < 10) removeElement("section-CPV")
+    //CARD 6 - EKOS
+    if (params.ekos <= 0) {
+      removeElement("img-Ekos")
+      removeElement("card-Ekos")
+    }
+    if (params.ekos < 10) removeElement("section-Ekos")
+    //CARD 7 - Kaiak
+    if (params.kaik <= 0) {
+      removeElement("img-Kaiak")
+      removeElement("card-Kaiak")
+    }
+    if (params.kaik < 10) removeElement("section-Kaiak")
+    //CARD 8 - REFIL
+    if (params.ref <= 0) {
+      removeElement("img-Refil")
+      removeElement("card-Refil")
+    }
+    if (params.ref < 10) removeElement("section-Refil")
+    //CARD 9 - AVON
+    if (params.avn <= 0) {
+      removeElement("img-Avon")
+      removeElement("card-Avon")
+    }
+    if (params.avn < 10) removeElement("section-Avon")
+
+    //CARD 10 - RECONHECIMENTO
+    if (params.hgh !== "sim") removeElement("section-Reconhecimento-hgh")
+    if (params.camp <= 0) removeElement("section-Reconhecimento-camp")
+    if (params.hgh !== "sim" && params.camp <= 0) {
+      removeElement("img-Reconhecimento")
+      removeElement("card-Reconhecimento")
+    }
+    if (params.camp == 1) {
+      document.getElementById("camp-label").textContent = "Campanha"
+    }
+
+    //CARD 11 - NIVEL
+    if (params.mov !== "SUBIU") {
+      removeElement("img-Nivel")
+      removeElement("card-Nivel")
+    }
+  }
+
+  // Executa as funções
+  populateSlides()
+  rulesShow()
+
+  // Função para converter o conteúdo do card em imagem
+  const printPanel = async () => {
+    const canvas = await html2canvas(document.body)
+    return canvas.toDataURL("image/png")
+  }
+
+  // Função para baixar a imagem
+  const downloadPrint = async (dataUrl) => {
+    const link = document.createElement("a")
+    const blob = await (await fetch(dataUrl)).blob()
+    const url = URL.createObjectURL(blob)
+    link.href = url
+    link.download = "content.png"
+    link.click()
+    URL.revokeObjectURL(url)
+  }
+
+  /* ===================================Modal logic=================================*/
+
+  const modal = document.getElementById("modal")
+  const closeModal = modal.querySelector(".close")
+  const shareButtons = document.querySelectorAll(".share-button")
+
+  let currentWrapper = null
+
+  shareButtons.forEach((button) => {
+    button.addEventListener("click", async (e) => {
+      // Adiciona botão de download para dispositivos móveis
+      if (!isMobile) {
+        const social = document.querySelector(".slider-nav")
+        social.style.display = "none"
+        const dataUrl = await printPanel()
+        await downloadPrint(dataUrl)
+        social.style.display = "flex"
+      } else {
+        currentWrapper = e.target.closest(".wrapper")
+        if (currentWrapper) {
+          openModal()
+        }
+      }
+    })
+  })
+
+  function openModal() {
+    modal.style.display = "flex"
+  }
+
+  closeModal.addEventListener("click", () => {
+    modal.style.display = "none"
+  })
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none"
+    }
+  })
+
+  /* ===================================EVENTOS=================================*/
+  const shareBtn = document.getElementById("modal-button-1")
+  // Adiciona evento de clique no botão de compartilhamento
+  shareBtn.addEventListener("click", async () => {
+    const social = document.querySelector(".slider-nav")
+    social.style.display = "none"
+    modal.style.display = "none"
+
+    const dataUrl = await printPanel()
+
+    if (navigator.share && isMobile) {
+      const response = await fetch(dataUrl)
+      const blob = await response.blob()
+      const file = new File([blob], "content.png", {
+        type: "image/png",
+      })
+      await navigator
+        .share({
+          title: "Retrospectiva",
+          text: "Confira este conteúdo!",
+          files: [file],
+        })
+        .then(() => {
+          console.log("Conteúdo compartilhado com sucesso")
+        })
+        .catch((error) => {
+          console.error("Erro ao compartilhar conteúdo:", error)
+        })
+    } else {
+      await downloadPrint(dataUrl)
+    }
+    social.style.display = "flex"
+  })
+
+  // Adiciona evento de clique no botão de download
+  const downloadBtn = document.getElementById("modal-button-2")
+  downloadBtn.addEventListener("click", async () => {
+    const social = document.querySelector(".slider-nav")
+    social.style.display = "none"
+    modal.style.display = "none"
+
+    const dataUrl = await printPanel()
+    await downloadPrint(dataUrl)
+
+    social.style.display = "flex"
+  })
+})
---
+++ final-version/js/script.js
@@ -0,0 +1,189 @@
+$(document).ready(function () {
+  var slideContainer = $(".slide-container")
+  var currentBgIndex = 0
+  var bgImages = $(".bg-container img")
+  var totalBgImages = bgImages.length
+  var animatedSlides = new Set() // Conjunto para rastrear slides já animados
+
+  function changeBackground(index) {
+    bgImages.removeClass("showbg")
+    bgImages.eq(index).addClass("showbg")
+  }
+
+  function updateImageVisibility(index) {
+    $(".natura-card__image img").removeClass("show")
+    $(".natura-card__image").eq(index).find("img").addClass("show")
+    $(".natura-card__image").eq(0).find("img").removeClass("show")
+    $(".natura-card__image").eq(0).find("img").addClass("primeiraimg")
+  }
+
+  // Timer para avançar automaticamente do primeiro slide
+  let firstSlideTimer // Variável para armazenar o timer do primeiro slide
+
+  // Após a inicialização do slider, configure o timer para o primeiro slide
+  slideContainer.on("init", function () {
+    firstSlideTimer = setTimeout(() => {
+      slideContainer.slick("slickNext") // Avança para o próximo slide
+    }, 10000) // 10 segundos (10000ms)
+  })
+
+  // Cancela o timer para os slides subsequentes
+  slideContainer.on(
+    "beforeChange",
+    function (event, slick, currentSlide, nextSlide) {
+      if (currentSlide === 0) {
+        clearTimeout(firstSlideTimer) // Cancela o timer ao sair do primeiro slide
+      }
+    }
+  )
+
+  // Função para animar os parágrafos e p
+  function animateWrapperParagraphs(wrapper) {
+    // Desabilita o botão de compartilhamento enquanto a animação ocorre
+    const buttonShare = wrapper
+      .closest(".wrapper")
+      .querySelector(".share-button")
+    if (buttonShare) {
+      buttonShare.disabled = true
+      buttonShare.querySelector(".sharebtnbase").style.background = "#ccc"
+    }
+    // Seleciona os p com a classe "destaque"
+    const destaqueParagraph = wrapper.querySelectorAll("p.destaque")
+    const paragraphs = Array.from(wrapper.querySelectorAll("p"))
+
+    /* document.querySelectorAll('p').forEach((p, index) => {
+        p.setAttribute('data-delay', 100 * (index + 1)); // Ajusta delays dinamicamente
+    });  */
+
+    // Ordena os elementos com base no atributo data-index
+    const sortedParagraphs = paragraphs.sort((a, b) => {
+      return (
+        parseInt(a.getAttribute("data-index"), 10) -
+        parseInt(b.getAttribute("data-index"), 10)
+      )
+    })
+
+    // Seleciona os outros elementos p
+    const otherElements = wrapper.querySelectorAll("p:not(.destaque)")
+
+    // Remove animações anteriores
+    destaqueParagraph.forEach((p) => p.classList.remove("animate"))
+    otherElements.forEach((el) => el.classList.remove("animate"))
+
+    // Se houver p com a classe "destaque", anima-os primeiro
+    if (destaqueParagraph.length > 0) {
+      // Anima os p com "destaque" primeiro, sem atraso entre eles
+      destaqueParagraph.forEach((p) => {
+        p.classList.add("animate")
+      })
+
+      // Anima os outros elementos após 1.5 segundos, em sequência
+      setTimeout(() => {
+        // Anima os elementos na ordem definida pelo data-index
+        sortedParagraphs.forEach((p, index) => {
+          // Variável para controlar o atraso acumulado
+          let accumulatedDelay = 0
+
+          // Anima os elementos considerando o delay definido no atributo data-delay
+          paragraphs.forEach((p) => {
+            const delay = parseInt(p.getAttribute("data-delay"), 10) || 300 // Atraso padrão: 300ms se não especificado
+            accumulatedDelay += delay // Soma ao atraso acumulado
+            setTimeout(() => {
+              p.classList.add("animate")
+            }, accumulatedDelay)
+          })
+          /*  setTimeout(() => {
+                p.classList.add("animate");
+            }, index * 300); */ // Ajuste o intervalo entre as animações (300ms neste caso)
+        })
+      }, 1500) // 1.5 segundos de atraso para os p "destaque"
+    } else {
+      // Se não houver p com "destaque", anima todos os outros elementos normalmente
+      sortedParagraphs.forEach((p, index) => {
+        // Variável para controlar o atraso acumulado
+        let accumulatedDelay = 0
+
+        // Anima os elementos considerando o delay definido no atributo data-delay
+        paragraphs.forEach((p) => {
+          const delay = parseInt(p.getAttribute("data-delay"), 10) || 300 // Atraso padrão: 300ms se não especificado
+          accumulatedDelay += delay // Soma ao atraso acumulado
+          setTimeout(() => {
+            p.classList.add("animate")
+          }, accumulatedDelay)
+        })
+        /*  setTimeout(() => {
+                    p.classList.add("animate");
+                }, index * 300); */ // Ajuste o intervalo entre as animações (300ms neste caso)
+      })
+    }
+    //habilita o botao quando animação acaba
+    setTimeout(() => {
+      if (buttonShare) {
+        buttonShare.disabled = false
+        buttonShare.querySelector(".sharebtnbase").style.background = "#fff"
+      }
+    }, 3000)
+  }
+
+  // Função para configurar os parágrafos no estado final (opacidade 1)
+  function finalizeParagraphs(wrapper) {
+    const paragraphs = wrapper.querySelectorAll("p")
+    paragraphs.forEach((p) => p.classList.add("animate"))
+  }
+
+  // Inicializar o slider
+  slideContainer.slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: true,
+    prevArrow: $(".slick-prev_custom"),
+    nextArrow: $(".slick-next_custom"),
+  })
+
+  // Configuração inicial
+  changeBackground(0)
+  updateImageVisibility(0)
+
+  // Evento para mudança de fundo e visibilidade
+  slideContainer.on(
+    "beforeChange",
+    function (event, slick, currentSlide, nextSlide) {
+      currentBgIndex = (nextSlide + totalBgImages) % totalBgImages
+      changeBackground(currentBgIndex)
+      updateImageVisibility(nextSlide)
+    }
+  )
+
+  // Evento para tratar a animação dos parágrafos
+  slideContainer.on("afterChange", function (event, slick, currentSlide) {
+    const activeWrapper = $(slick.$slides[currentSlide]).find(".natura-card")[0]
+
+    if (activeWrapper && !animatedSlides.has(currentSlide)) {
+      // Animação apenas se o slide ainda não foi animado
+      animateWrapperParagraphs(activeWrapper)
+
+      // Marca o slide como animado
+      animatedSlides.add(currentSlide)
+    } else if (activeWrapper) {
+      // Configura o estado final diretamente caso o slide já tenha sido animado
+      finalizeParagraphs(activeWrapper)
+    }
+  })
+
+  // Modal logic
+
+  // Configuração inicial: anima o primeiro slide ao carregar
+  setTimeout(() => {
+    const firstWrapper = $(
+      ".slide-container .slick-slide.slick-active .natura-card"
+    )[0]
+    if (firstWrapper) {
+      animateWrapperParagraphs(firstWrapper)
+
+      // Marca o primeiro slide como animado
+      animatedSlides.add(0)
+    }
+  }, 0)
+})
